c6b0c8a66fd336ea68c5bdc4bc642c3386cc9c69
Evgenny Kalchev <bugiardolr@gmail.com>
2019-02-06 13:27:46 +0200

    Added shutdown hook.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    //TODO Add Non-blocking kafka consumer to save the messages to the DB
+    private static ConfigurableApplicationContext context;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        addShutdownHook();
     }
 
+    private static void addShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            context.close();
+        }));
+    }
 }
 
