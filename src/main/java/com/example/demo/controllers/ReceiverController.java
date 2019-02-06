db4872ef2ded2d1bee71997de81b3b7dc5bb17b2
Evgenny Kalchev <bugiardolr@gmail.com>
2019-02-06 21:49:02 +0200

    Fix typo in request mapping of ReceiverController.

--- src/main/java/com/example/demo/controllers/ReceiverController.java
+++ src/main/java/com/example/demo/controllers/ReceiverController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/recieve")
+@RequestMapping("/receive")
 public class ReceiverController {
 
     private Publisher publisher;
