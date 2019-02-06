cf93f6317a48c95210e901a79e62b416a27bbab1
Evgenny Kalchev <bugiardolr@gmail.com>
2019-02-06 20:42:56 +0200

    Refactor util class method names.

--- src/main/java/com/example/demo/util/Util.java
+++ src/main/java/com/example/demo/util/Util.java
@@ -9,11 +9,10 @@ import java.util.List;
 public class Util {
     // Transfer data between entities and data objects.
 
-    public static List<InformationEntity> toEntity(List<Information> data){
+    public static List<InformationEntity> toInformationEntityList(List<Information> data){
         List<InformationEntity> result = new ArrayList<>();
 
         for (Information inf : data){
-            // Checks?
             InformationEntity temp = new InformationEntity();
             temp.setData(inf.getData());
             result.add(temp);
@@ -22,11 +21,10 @@ public class Util {
         return result;
     }
 
-    public static List<Information> toModel(List<InformationEntity> data){
+    public static List<Information> toInformationList(List<InformationEntity> data){
         List<Information> result = new ArrayList<>();
 
         for (InformationEntity inf : data){
-            // Checks?
             Information temp = new Information();
             temp.setData(inf.getData());
             result.add(temp);
